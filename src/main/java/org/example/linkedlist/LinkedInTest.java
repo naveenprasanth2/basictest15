d4768f613c4be8c515ee86dcc7cef8b39e1598f9
naveen prasanth <naveenprasanth12@gmail.com>
2024-06-15 16:23:36 +0530

    linkedlist

---
+++ src/main/java/org/example/linkedlist/LinkedInTest.java
@@ -0,0 +1,10 @@
+package org.example.linkedlist;
+
+public class LinkedInTest {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList(5);
+        list.append(10);
+        list.append(15);
+        list.print();
+    }
+}
--- src/main/java/org/example/linkedlist/LinkedList.java
+++ src/main/java/org/example/linkedlist/LinkedList.java
@@ -1,4 +1,49 @@
 package org.example.linkedlist;
 
 public class LinkedList {
+    static class Node {
+        int value;
+        Node next = null;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    Node head;
+    Node tail;
+    int length = 0;
+
+    public LinkedList(int value) {
+        Node node = new Node(value);
+        head = tail = node;
+        length++;
+    }
+
+    public void append(int val) {
+        Node newNode = new Node(val);
+        if (head == null && tail == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+
+    public void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+    }
+
+    public void printHead() {
+        System.out.println(head.value);
+    }
+
+    public void printTail() {
+        System.out.println(tail.value);
+    }
 }
