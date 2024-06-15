9ec005729d4dd36126d32e58567bdea9fef868a7
naveen prasanth <naveenprasanth12@gmail.com>
2024-06-15 17:43:37 +0530

    binary search tree

---
+++ src/main/java/org/example/binary/BinarySearchTree.java
@@ -0,0 +1,43 @@
+package org.example.binary;
+
+public class BinarySearchTree {
+    Node root;
+
+    static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                } else {
+                    temp = temp.left;
+                }
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                } else {
+                    temp = temp.right;
+                }
+            }
+        }
+    }
+}
