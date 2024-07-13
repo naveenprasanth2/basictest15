c2639c2c397f806145d7655f84befa59a0d73c0c
naveen prasanth <naveenprasanth12@gmail.com>
2024-07-13 14:44:17 +0530

    Create FindUncommon.java

---
+++ src/main/java/org/example/synechron/FindUncommon.java
@@ -0,0 +1,10 @@
+package org.example.synechron;
+
+public class FindUncommon {
+    static String str1 = "characters";
+    static String str2 = "alphabets";
+
+    public static void main(String[] args) {
+
+    }
+}
