efc8fbff3be97d1ef14aca3426f2aae5d6d73096
naveen prasanth <naveenprasanth2@gmail.com>
2024-11-14 21:21:40 +0530

    finding duplicates using memory efficient way

--- src/main/java/org/example/codedecode/challenges/FindDuplicateWithBool.java
+++ src/main/java/org/example/codedecode/challenges/FindDuplicateWithBool.java
@@ -1,4 +1,16 @@
 package org.example.codedecode.challenges;
 
 public class FindDuplicateWithBool {
+    public static void main(String[] args) {
+        String str1 = "characters";
+        boolean[] booleans = new boolean[256];
+        char[] chars = str1.toCharArray();
+        for (char c : chars){
+            if (booleans[c]){
+                System.out.println(c);
+            }else {
+                booleans[c] = true;
+            }
+        }
+    }
 }
