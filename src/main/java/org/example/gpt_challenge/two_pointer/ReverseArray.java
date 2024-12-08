c7ea916279bf92851743a252680804c04a664ce9
naveen prasanth <naveenprasanth2@gmail.com>
2024-12-08 07:43:46 +0530

    reversing array using 2 pointer approach

---
+++ src/main/java/org/example/gpt_challenge/two_pointer/ReverseArray.java
@@ -0,0 +1,20 @@
+package org.example.gpt_challenge.two_pointer;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int left = 0;
+        int right = a.length - 1;
+        while (left < right){
+            int temp = a[left];
+            a[left] = a[right];
+            a[right] = temp;
+            left++;
+            right--;
+        }
+
+        System.out.println(Arrays.toString(a));
+    }
+}
