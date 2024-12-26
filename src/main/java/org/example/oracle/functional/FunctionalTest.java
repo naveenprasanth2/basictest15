f402d0292c3b95cdcbf787a1d614830a8e97cd0e
naveen prasanth <naveenprasanth2@gmail.com>
2024-12-26 15:28:26 +0530

    functional interface changes

--- src/main/java/org/example/oracle/functional/FunctionalTest.java
+++ src/main/java/org/example/oracle/functional/FunctionalTest.java
@@ -1,9 +1,20 @@
 package org.example.oracle.functional;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class FunctionalTest {
     public static void main(String[] args) {
         Test test = new Test();
         FunctionalInterfaceDemo demo = test::test;
         demo.summa();
+
+      Supplier<Stream<String>> supplier = () -> Stream.of("Summa");
+        supplier.get().forEach(System.out::println);
+
+        IntFunction<String> intFunction = String::valueOf;
+        System.out.println(intFunction.apply(657_676_576));
     }
 }
